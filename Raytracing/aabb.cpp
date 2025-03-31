152e59edfd460693c59fee2405b2a1c7d744ec05
Zachary Ng <zachn716@gmail.com>
2025-03-31 10:32:10 -0700

    aabb improvements
    https://tavianator.com/2022/ray_box_boundary.html

--- Raytracing/aabb.cpp
+++ Raytracing/aabb.cpp
@@ -1,5 +1,13 @@
 #include "aabb.h"
 
+static inline float min(float x, float y) {
+	return x < y ? x : y;
+}
+
+static inline float max(float x, float y) {
+	return x > y ? x : y;
+}
+
 aabb::aabb(const pos3* points, int count) {
 	float min_x = INFINITY;
 	float min_y = INFINITY;
@@ -10,28 +18,16 @@ aabb::aabb(const pos3* points, int count) {
 
 	for (int i = 0; i < count; i++) {
 		const pos3& point = points[i];
-		if (point.x() < min_x) {
-			min_x = point.x();
-		}
-		if (point.x() > max_x) {
-			max_x = point.x();
-		}
-		if (point.y() < min_y) {
-			min_y = point.y();
-		}
-		if (point.y() > max_y) {
-			max_y = point.y();
-		}
-		if (point.z() < min_z) {
-			min_z = point.z();
-		}
-		if (point.z() > max_z) {
-			max_z = point.z();
-		}
+		min_x = min(min_x, point.x());
+		min_y = min(min_y, point.y());
+		min_z = min(min_z, point.z());
+		max_x = max(max_x, point.x());
+		max_y = max(max_y, point.y());
+		max_z = max(max_z, point.z());
 	}
 
-	position_min = pos3(min_x, min_y, min_z);
-	position_max = pos3(max_x, max_y, max_z);
+	corners[0] = pos3(min_x, min_y, min_z);
+	corners[1] = pos3(max_x, max_y, max_z);
 }
 
 inline bool between(float low, float high, float test) {
@@ -39,42 +35,51 @@ inline bool between(float low, float high, float test) {
 }
 
 bool aabb::point_inside(const pos3& point) const {
-	return between(position_min.x(), position_max.x(), point.x()) &&
-		between(position_min.y(), position_max.y(), point.y()) &&
-		between(position_min.z(), position_max.z(), point.z());
+	return between(corners[0].x(), corners[1].x(), point.x()) &&
+		between(corners[0].y(), corners[1].y(), point.y()) &&
+		between(corners[0].z(), corners[1].z(), point.z());
 }
 
 bool aabb::intersects_aabb(const aabb& other) const {
-	return (position_min.x() <= other.position_max.x() && position_max.x() >= other.position_min.x()) &&
-		(position_min.y() <= other.position_max.y() && position_max.y() >= other.position_min.y()) &&
-		(position_min.z() <= other.position_max.z() && position_max.z() >= other.position_min.z());
+	return (corners[0].x() <= other.corners[1].x() && corners[1].x() >= other.corners[0].x()) &&
+		(corners[0].y() <= other.corners[1].y() && corners[1].y() >= other.corners[0].y()) &&
+		(corners[0].z() <= other.corners[1].z() && corners[1].z() >= other.corners[0].z());
 }
 
-// https://tavianator.com/2011/ray_box.html
+// https://tavianator.com/2011/ray_box.html, https://tavianator.com/2022/ray_box_boundary.html
 bool aabb::ray_intersects(const ray& r) const {
 	const pos3& ray_origin = r.origin();
 	const vec3 ray_direction_inv = vec3(1 / r.direction().x(), 1 / r.direction().y(), 1 / r.direction().z());
+	float tmin = 0.0;
+	float tmax = INFINITY;
 
-	float tx1 = (position_min.x() - ray_origin.x()) * ray_direction_inv.x();
-	float tx2 = (position_max.x() - ray_origin.x()) * ray_direction_inv.x();
-	float tmin = std::min(tx1, tx2);
-	float tmax = std::max(tx1, tx2);
-	if (tmax < tmin) {
-		return false;
+	/*
+	for (int d = 0; d < 3; d++) {
+		bool sign = signbit(ray_direction_inv[d]);
+		float dmin = (corners[sign][d] - ray_origin[d]) * ray_direction_inv[d];
+		float dmax = (corners[!sign][d] - ray_origin[d]) * ray_direction_inv[d];
+		tmin = max(dmin, tmin);
+		tmax = min(dmax, tmax);
 	}
+	*/
 
-	float ty1 = (position_min.y() - ray_origin.y()) * ray_direction_inv.y();
-	float ty2 = (position_max.y() - ray_origin.y()) * ray_direction_inv.y();
-	tmin = std::max(tmin, std::min(ty1, ty2));
-	tmax = std::min(tmax, std::max(ty1, ty2));
-	if (tmax < tmin) {
-		return false;
-	}
-
-	float tz1 = (position_min.z() - ray_origin.z()) * ray_direction_inv.z();
-	float tz2 = (position_max.z() - ray_origin.z()) * ray_direction_inv.z();
-	tmin = std::max(tmin, std::min(tz1, tz2));
-	tmax = std::min(tmax, std::max(tz1, tz2));
+	bool sign = signbit(ray_direction_inv[0]);
+	float dmin = (corners[sign][0] - ray_origin[0]) * ray_direction_inv[0];
+	float dmax = (corners[!sign][0] - ray_origin[0]) * ray_direction_inv[0];
+	tmin = max(dmin, tmin);
+	tmax = min(dmax, tmax);
+	
+	sign = signbit(ray_direction_inv[1]);
+	dmin = (corners[sign][1] - ray_origin[1]) * ray_direction_inv[1];
+	dmax = (corners[!sign][1] - ray_origin[1]) * ray_direction_inv[1];
+	tmin = max(dmin, tmin);
+	tmax = min(dmax, tmax);
+	
+	sign = signbit(ray_direction_inv[2]);
+	dmin = (corners[sign][2] - ray_origin[2]) * ray_direction_inv[2];
+	dmax = (corners[!sign][2] - ray_origin[2]) * ray_direction_inv[2];
+	tmin = max(dmin, tmin);
+	tmax = min(dmax, tmax);
 
 	return tmax >= tmin;
 }
--- Raytracing/aabb.h
+++ Raytracing/aabb.h
@@ -6,16 +6,15 @@
 
 class aabb {
 public:
-	aabb() : position_min(pos3(0, 0, 0)), position_max(pos3(0, 0, 0)) {}
-	aabb(pos3 min, pos3 max) : position_min(min), position_max(max) {}
-	aabb(float min_x, float min_y, float min_z, float max_x, float max_y, float max_z) : position_min(pos3(min_x, min_y, min_z)), position_max(pos3(max_x, max_y, max_z)) {}
+	aabb() : corners { pos3(0, 0, 0), pos3(0, 0, 0) } {}
+	aabb(pos3 min, pos3 max) : corners { min, max } {}
+	aabb(float min_x, float min_y, float min_z, float max_x, float max_y, float max_z) : corners { pos3(min_x, min_y, min_z), pos3(max_x, max_y, max_z) } {}
 	aabb(const pos3* points, int count);
 
 	bool point_inside(const pos3& point) const;
 	bool intersects_aabb(const aabb& other) const;
 	bool ray_intersects(const ray& r) const;
 protected:
-	pos3 position_min;
-	pos3 position_max;
+	pos3 corners[2];
 };
 
